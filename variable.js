2703942986b9de8e5898295196ebe664b83c179e
Md. Abu Bakker Siddique <bakker311042@gmail.com>
2023-03-15 12:08:16 +0600

    Variable declare

---
+++ variable.js
@@ -0,0 +1,13 @@
+var name = "Abu";
+var name = "siddique"; //re-declare
+name = "Bakker"; // Reasign/re-declare
+
+let nameLet = "Abu";
+nameLet = "Bakker"; // Reasign/not use re-declare
+
+const nameConst = "Abu";
+// nameConst = "Bakker"; // Not use Reasign/ not use re-declare
+
+console.log(name);
+console.log(nameLet);
+console.log(nameConst);
