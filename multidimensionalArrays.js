0a523b99856598ed4dc2d972acff02b438691f60
Md. Abu Bakker Siddique <bakker311042@gmail.com>
2023-03-16 16:10:11 +0600

    multidimensionalArrays

---
+++ multidimensionalArrays.js
@@ -0,0 +1,11 @@
+//multidimensionalArrays
+
+var aA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var bB = [11, 22, 33, 44, 55, 66, 77, 88, 99, 10];
+
+var cC = ["A", "B", "C", "D"];
+
+var total = [aA, bB, cC]
+
+console.log(total)
+console.log(total[2][1])
