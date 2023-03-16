0f2dd9363989a8e294d021bc7fcd58da99d0da5c
Md. Abu Bakker Siddique <bakker311042@gmail.com>
2023-03-16 17:36:45 +0600

    arraySet

---
+++ arraySet.js
@@ -0,0 +1,21 @@
+//arraySet duplicate value not count
+
+var aSet = new Set();
+
+aSet.add("Bangladesh")
+aSet.add("India")
+aSet.add("US")
+aSet.add("UK")
+aSet.add("Pakistan")
+aSet.add("Nepal")
+aSet.add("Chaina")
+aSet.add("India")
+aSet.add("Chaina")
+
+
+console.log("Full set: ")
+console.log(aSet)
+
+
+var bSet = new Set(['a', 'a', "b", 'c', "d"]);
+console.log(bSet)
