cdb31d149426679f32abc4a38e39b56dad0b564b
Md. Abu Bakker Siddique <bakker311042@gmail.com>
2023-03-16 18:44:25 +0600

    Create Class & Object

---
+++ class.js
@@ -0,0 +1,23 @@
+// class
+class TestClass {
+    MyName() {
+        console.log("My name is bakker")
+    }
+
+    MyA() {
+        console.log("Chapai nawabganj")
+    }
+
+    MyC() {
+        console.log("Bangladesh")
+    }
+
+    MyX(X) {
+        console.log(X)
+    }
+}
+
+var obj = new TestClass;
+
+obj.MyName()
+obj.MyX("Nayagola")
