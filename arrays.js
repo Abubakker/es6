430ff92ca8448ab92f90b92045fb93962692a949
Md. Abu Bakker Siddique <bakker311042@gmail.com>
2023-03-16 16:05:22 +0600

    arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+//arrays
+var aA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var bB = new Array(11, 22, 33, 44, 55, 66, 77, 88, 99, 10);
+
+for (let item of aA) {
+    console.log(item)
+}
+
+for (let i of bB) {
+    console.log(i)
+}
