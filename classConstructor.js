7d1ac73283c1bf0d81ea5029686c351e0149e863
Md. Abu Bakker Siddique <bakker311042@gmail.com>
2023-03-20 10:51:45 +0600

    classConstructor

---
+++ classConstructor.js
@@ -0,0 +1,27 @@
+// classConstructor
+class TestClass {
+    constructor() {
+        console.log("constructor");
+    }
+
+    MyName() {
+        console.log("My name is bakker");
+    }
+
+    MyA() {
+        console.log("Chapai nawabganj");
+    }
+
+    MyC() {
+        console.log("Bangladesh");
+    }
+
+    MyX(X) {
+        console.log(X);
+    }
+}
+
+var obj = new TestClass;
+
+obj.MyName()
+obj.MyX("Nayagola")
